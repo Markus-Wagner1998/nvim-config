b37348167e3177b5b8818e043494b6b012185141
Markus Wagner <markus.wagner1998@gmail.com>
2024-04-12 16:43:53 +0200

    Initial Neovim setup

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>")
+    vim.keymap.set("n", "<leader>m", ":Neotree action=close<CR>")
+    require("neo-tree").setup({
+      window = {
+        mappings = {
+          ["P"] = { "toggle_preview" },
+        }
+      }
+    })
+  end
+}
