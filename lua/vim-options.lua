fa37d59b8e7a2147eaf7a3749b8d2953a30f4650
Markus Wagner <markus.wagner1998@gmail.com>
2024-04-12 21:41:35 +0200

    Adding copying to system clipboard using <leader><

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,3 +2,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.api.nvim_set_keymap('n', '<Leader>y', '"+y', {noremap = true})
+vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', {noremap = true})
+
