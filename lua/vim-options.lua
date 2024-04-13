21a29296b39003766f290a74f984ec9ace92eaf3
Markus Wagner <markus.wagner1998@gmail.com>
2024-04-13 12:05:07 +0200

    Added shortcuts for vertical and horizontal ui split

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.api.nvim_set_keymap('n', '<Leader>y', '"+y', {noremap = true})
 vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', {noremap = true})
 
+vim.api.nvim_set_keymap('n', '<Leader>v', ':vsplit<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>s', ':split<cr>', {noremap = true})
+
+vim.wo.number = true
