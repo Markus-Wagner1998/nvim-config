2f1a5ff9e49ebd19c95489612a467fc2b08dab23
Markus Wagner <markus.wagner@consol.de>
2024-04-16 21:53:43 +0200

    Telescope adjustment for large projects and files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,19 +7,49 @@ return {
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ap", ":Telescope find_files no_ignore=true<cr>", {})
       vim.keymap.set("v", "<leader>p", '"zy:Telescope find_files search_file=<C-r>z<cr>', {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>ff", ':Telescope live_grep glob_pattern=*.{js,ts,java,sh}<cr>', {})
       vim.keymap.set("v", "<leader>fg", '"zy:Telescope live_grep default_text=<C-r>z<cr>', {})
     end
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
+      local function filenameFirst(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == "." then return tail end
+        return string.format("%s\t\t%s", tail, parent)
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "TelescopeResults",
+        callback = function(ctx)
+          vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+          end)
+        end,
+      })
+
       require("telescope").setup {
+        pickers = {
+          find_files = { path_display = filenameFirst, },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown {
             }
+          }
+        },
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules", "build", "dist", "yarn.lock"
+          },
+          {
+            path_display = { "smart" }
           }
         }
       }
