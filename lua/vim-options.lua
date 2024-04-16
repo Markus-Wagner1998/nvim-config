4998498785fa1cdef381b7078a44eb3d7753fcae
Markus Wagner <markus.wagner@consol.de>
2024-04-16 21:54:04 +0200

    Shortcuts for Vim Splits

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,8 +6,8 @@ vim.cmd("set shiftwidth=2")
 vim.api.nvim_set_keymap('n', '<Leader>y', '"+y', {noremap = true})
 vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', {noremap = true})
 
-vim.api.nvim_set_keymap('n', '<Leader>v', ':vsplit<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<Leader>s', ':split<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>v', ':vsplit<cr><C-w>l', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>s', ':split<cr><C-w>j', {noremap = true})
 
 vim.wo.number = true
 vim.wo.relativenumber = true
