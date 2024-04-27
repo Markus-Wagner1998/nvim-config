e616e67a61c18b64121c3b37529aa660839b02eb
Markus Wagner <markus.wagner@consol.de>
2024-04-27 21:11:12 +0200

    Tab Handling shortcuts

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,5 +9,7 @@ vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>v', ':vsplit<cr><C-w>l', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>s', ':split<cr><C-w>j', {noremap = true})
 
+vim.api.nvim_set_keymap('n', '<Leader>t', ':tabnew<cr>', {noremap = true})
+
 vim.wo.number = true
 vim.wo.relativenumber = true
