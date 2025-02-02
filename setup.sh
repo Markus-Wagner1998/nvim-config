b4b249a72be05a78d6c0cf5bebf10824203bab00
Markus Wagner <MWagner@pcs.com>
2025-02-02 18:07:45 +0100

    Add instructions for new jdtls version and Lombok Support

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 brew install ripgrep
+# Java 21 must be installed
+# Lombok must be available
+# Set nvim alias like:
+# alias nvim="JAVA_HOME=/Users/mwagner/.sdkman/candidates/java/21.0.3-graal/ JDTLS_JVM_ARGS="-javaagent:/Users/mwagner/.local/share/nvim/mason/packages/jdtls/lombok.jar" nvim"
