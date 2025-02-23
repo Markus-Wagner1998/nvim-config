4e244c53809bb6d846aab02a197a3e9e79db379e
Markus Wagner <markus.wagner1998@gmail.com>
2025-02-23 15:58:55 +0100

    Adjusted Lualine theming

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,9 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     require('lualine').setup({
-      theme = "dracula"
+      options = {
+        theme = "onedark"
+      }
     })
   end
 }
