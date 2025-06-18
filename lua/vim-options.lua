d7421d291f69d6c8fb3e0974fe625646525f482d
Markus Wagner <MWagner@pcs.com>
2025-06-18 07:55:33 +0200

    Added system clipboard paste shortcut

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,8 +6,8 @@ vim.cmd("set shiftwidth=4")
 vim.api.nvim_set_keymap('n', '<Leader>y', '"+y', {noremap = true})
 vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', {noremap = true})
 
-vim.api.nvim_set_keymap('n', '<Leader>p', '"+p', {noremap = true})
-vim.api.nvim_set_keymap('v', '<Leader>p', '"+p', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-p>', '"+p', {noremap = true})
+vim.api.nvim_set_keymap('v', '<C-p>', '"+p', {noremap = true})
 
 vim.api.nvim_set_keymap('n', '<Leader>v', ':vsplit<cr><C-w>l', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>s', ':split<cr><C-w>j', {noremap = true})
